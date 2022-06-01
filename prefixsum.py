cc0c436a9172ab50a9de59ae602ea392f4379d3c
Michael0cean <michael.ocean@gmail.com>
2022-06-01 17:41:27 +0300

    prefixsum.py added

---
+++ prefixsum.py
@@ -0,0 +1,18 @@
+"""
+Here is a short description of the script:
+    Enter your name and this script will randomly generate 4-digit PIN code
+"""
+
+__author__ = "Mikhail Okunev"
+__author_email__ = "Mikhail.Okunev@gmail.com"
+
+import os
+
+def main():
+    
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
