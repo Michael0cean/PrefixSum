0f35d1e470e949d0cc8820fe6889b16afec42768
Michael0cean <michael.ocean@gmail.com>
2022-06-01 18:36:34 +0300

    launch.json added

--- prefixsum.py
+++ prefixsum.py
@@ -1,16 +1,27 @@
 """
 Here is a short description of the script:
-    Enter your name and this script will randomly generate 4-digit PIN code
+    execute the scripy:
+        python prefixsum.py <filename with prefixes>
+    where
+        <filename with prefixes> - path + filename of the file with the list of
+                                    prefixes to summarise in larger blocks in format
+                                    IP network <space> netmask
+                                    or
+                                    IP network / prefix
+
 """
 
 __author__ = "Mikhail Okunev"
 __author_email__ = "Mikhail.Okunev@gmail.com"
 
 import os
+import sys
 
 def main():
-    
-
+    if len(sys.argv) != 1:
+        print('python prefixsum.py <filename with prefixes>')
+    else: 
+        print('Try to open ',sys.argv[0])
     return 0
 
 
